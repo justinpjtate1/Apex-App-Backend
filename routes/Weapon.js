d05cf3741dae2a0a4c0c5466d7abc2e62c66b8e1
justintate <justinpjtate1@gmail.com>
2023-02-08 15:20:29 +0000

    Added weapon routes

---
+++ routes/Weapon.js
@@ -0,0 +1,36 @@
+const { response } = require('express');
+const express = require('express');
+
+const Weapon = require('../models/Weapon');
+
+const router = express.Router();
+
+// Action: Index
+// Method: GET
+// URI: /weapons
+// Description: Get All Weapons
+
+router.get('/weapons', (req, res) => {
+    Weapon.find()
+    .then((weapons) => {
+        res.status(200).json({ weapons: weapons});
+    })
+    .catch((error) => {
+        res.status(500).json({ error: error });
+    })
+})
+
+// Action: Index
+// Method: GET
+// URI: /weapons/:id
+// Description: Get a specific weapon
+
+router.get('/weapons/:id', (req, res) => {
+    Weapon.findById(req.params.id)
+    .then((weapon) => {
+        res.status(200).json({ weapon: weapon });
+    })
+    .catch((error) => {
+        res.status(500).json({ error: error });
+    })
+})
